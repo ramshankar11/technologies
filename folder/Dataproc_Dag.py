d3c7b815b4847bd740a40ff945defb2b83996281
ramshankar11 <74725135+ramshankar11@users.noreply.github.com>
2022-09-18 13:11:03 +0530

    Update Dataproc_Dag.py

--- folder/Dataproc_Dag.py
+++ folder/Dataproc_Dag.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from airflow.operators.dummy_operator import DummyOperator
 PROJECT_ID='studious-lore-344410'
 REGION='us-west1'
+
 CLUSTER_NAME='test-from-composer'
 with DAG('dataproc_job_create_submit_delete', description='Dataproc something DAG',
           schedule_interval=None,
